d8f29441d20a2f69e021f4efa05e6b160d75944e
SelfGrowth <singhvanshita898@gmail.com>
2022-04-01 20:52:08 +0530

    Add index.js

---
+++ headings_vs/js/index.js
@@ -0,0 +1,9 @@
+/**
+ * @package Headings VS
+ * @author Vanshita Singh https://github.com/Vanshitasingh
+ */
+
+ window.onload = (event) => {
+    // Display the alert
+    alert('Hello World!');
+}
